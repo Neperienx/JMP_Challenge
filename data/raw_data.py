7370dd9c775902fdaef22a44ebbe4055d682a9f7
Rachid Essmoum <ge75hin@mytum.de>
2023-05-13 22:39:31 +0200

    loading data

--- data/raw_data.py
+++ data/raw_data.py
@@ -4,6 +4,8 @@ sys.path.append('../nuscenes-devkit-master')
 from nuscenes.nuscenes import NuScenes
 from nuscenes.utils.data_classes import LidarPointCloud
 import sys
+import open3d as o3d
+from PIL import Image
 sys.path.append('../nuscenes-devkit-master')
 
 def load_data(data_path):
@@ -20,7 +22,7 @@ def load_data(data_path):
     nusc = NuScenes(version='v1.0-mini', dataroot=data_path, verbose=True)
     
     # Loop through each sample in the dataset
-    for sample in nusc.sample_iter():
+    for sample in nusc.sample:
         lidar_data = []
         radar_data = []
         camera_data = []
@@ -34,16 +36,17 @@ def load_data(data_path):
         for sensor_name in ['RADAR_FRONT', 'RADAR_FRONT_LEFT', 'RADAR_FRONT_RIGHT', 'RADAR_BACK_LEFT', 'RADAR_BACK_RIGHT']:
             radar_token = sample['data'][sensor_name]
             radar_path, boxes, camera_intrinsic = nusc.get_sample_data(radar_token)
-            radar_data.append(np.load(radar_path)['arr_0'])
+            radar_data.append(np.asarray(o3d.io.read_point_cloud(radar_path)))
         
         # Get the camera data for the current sample
         for sensor_name in ['CAM_FRONT', 'CAM_FRONT_LEFT', 'CAM_FRONT_RIGHT', 'CAM_BACK_LEFT', 'CAM_BACK_RIGHT']:
             camera_token = sample['data'][sensor_name]
             camera_path, boxes, camera_intrinsic = nusc.get_sample_data(camera_token)
-            camera_data.append(np.load(camera_path)['arr_0'])
+            camera_data.append(np.array(Image.open(camera_path)))
         
         # Combine the lidar, radar, and camera data into one sample
-        sample_data = np.concatenate((lidar_data, np.concatenate(radar_data, axis=1), np.concatenate(camera_data, axis=1)), axis=1)
-        dataset.append(sample_data)
+        #sample_data = np.concatenate((lidar_data, np.concatenate(radar_data, axis=1), np.concatenate(camera_data, axis=1)), axis=1)
+        #dataset.append(sample_data)
     
-    return np.array(dataset)
+    #return np.array(dataset)
+    return lidar_data, radar_data, camera_data
--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from data.raw_data import load_data
 
 if __name__ == '__main__':
     # Replace <data_path> with the path to your raw sensor data
-    data_path = '<data_path>'
+    data_path = '/home/rachid/JmpChallenge/JMP_Challenge_Repo/data/raw-data/nuscenes'
 
     # Load the raw sensor data
-    raw_sensor_data = load_data(data_path)
+    rawlidar_data, radar_data, camera_data_sensor_data = load_data(data_path)
 
     # Print the shape of the raw sensor data
-    print('Raw sensor data shape:', raw_sensor_data.shape)
+    print('Raw sensor data shape loaded')
